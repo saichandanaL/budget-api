678a45c818a320cc38c2974c4e6ddb3ec44d783c
Super user <Super user@TeamCity>
2023-04-25 07:15:47 +0000

    TeamCity change in 'Budget Api (1)' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.nuGetInstaller
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -46,6 +47,10 @@ object Build : BuildType({
             projects = "budget-api.sln"
             updatePackages = updateParams {
             }
+        }
+        script {
+            name = "dotnet build"
+            scriptContent = "dotnet build -o ./build-output"
         }
     }
 
